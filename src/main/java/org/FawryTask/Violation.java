8e634171893900a114f957ca5d577ecd2d5d6b9e
MmoaazM <imoaaz29@gmail.com>
2026-07-24 17:59:57 +0300

    finish Violation class

--- src/main/java/org/FawryTask/Violation.java
+++ src/main/java/org/FawryTask/Violation.java
@@ -1,4 +1,17 @@
 package org.FawryTask;
 
 public class Violation {
+    private final String ruleName; // make it constant after assigned
+    private final String message;
+    private final double fee;
+
+    public Violation(String ruleName, String message, double fee) {
+        this.ruleName = ruleName;
+        this.message = message;
+        this.fee = fee;
+    }
+
+    public String getRuleName() { return ruleName; }
+    public String getMessage() { return message; }
+    public double getFee() { return fee; }
 }
